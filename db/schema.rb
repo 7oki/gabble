658673f48346703e05887b84f224cfd7b5b09032
iwakiriK <balssearch0113@gmail.com>
2019-01-04 05:44:12 +0000

    [update]railsチュートリアル3章までの内容

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190104051708) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "login_name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Gabble"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
