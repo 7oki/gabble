a7233dfb5c257809c5ba7b4d9830be6351ea9d3f
iwakiriK <balssearch0113@gmail.com>
2019-01-09 20:40:52 +0000

    [update]railsチュートリアル10章までの内容

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190109155957) do
+ActiveRecord::Schema.define(version: 20190109202634) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190109155957) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["login_name"], name: "index_users_on_login_name", unique: true
   end
 
